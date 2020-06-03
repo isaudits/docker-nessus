d87aa0f84a4a095bef6abe958bf398ef8ac226c0
Matthew C. Jones, CPA, CISA, OSCP, CCFE <mjones@isaudits.com>
2020-06-03 17:43:58 -0400

    initial commit

---
+++ docker/root/defaults/update-nessus.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/with-contenv bash
+
+# only run update at startup if scanner initialization is complete
+if [ -f /opt/nessus/var/nessus/first_run ]; then
+    
+    CHECK=$(/opt/nessus/sbin/nessuscli fetch --check)
+    
+    if [ -n "${LICENSE}" ];then
+        echo "-- Registering as a Nessus Pro scanner"
+        /opt/nessus/sbin/nessuscli fetch --register "${LICENSE}"
+    elif [ -n "${SECURITYCENTER}" ];then
+        echo "-- Registering as a SecurityCenter-linked scanner"
+        /opt/nessus/sbin/nessuscli fetch --security-center
+    fi
+    
+fi
