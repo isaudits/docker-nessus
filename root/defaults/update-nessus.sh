7522e7a4100ab05fc4ad2811f18f974712012b8a
Matthew C. Jones, CPA, CISA, OSCP, CCFE <mjones@isaudits.com>
2020-06-03 21:33:24 -0400

    remove unused license check

--- root/defaults/update-nessus.sh
+++ root/defaults/update-nessus.sh
@@ -3,8 +3,6 @@
 # only run update at startup if scanner initialization is complete
 if [ -f /opt/nessus/var/nessus/first_run ]; then
     
-    CHECK=$(/opt/nessus/sbin/nessuscli fetch --check)
-    
     if [ -n "${LICENSE}" ];then
         echo "-- Registering as a Nessus Pro scanner and downloading updates"
         /opt/nessus/sbin/nessuscli fetch --register "${LICENSE}"
