40c6beae51d878a8fd827aee3f469bb4af47138e
Matthew C. Jones, CPA, CISA, OSCP, CCFE <mjones@isaudits.com>
2020-12-01 14:47:31 -0500

    fix update script to work if updates are disabled

--- root/defaults/update-nessus.sh
+++ root/defaults/update-nessus.sh
@@ -10,9 +10,11 @@ else
     if [ -n "${LICENSE}" ];then
         echo "-- Registering as a Nessus Pro scanner and downloading updates"
         /opt/nessus/sbin/nessuscli fetch --register "${LICENSE}"
+        /opt/nessus/sbin/nessuscli update --all
     elif [ -n "${SECURITYCENTER}" ];then
-        echo "-- Registering as a SecurityCenter-linked scanner"
+        echo "-- Registering as a SecurityCenter-linked scanner and downloading updates"
         /opt/nessus/sbin/nessuscli fetch --security-center
+        /opt/nessus/sbin/nessuscli update --all
     fi
     
 fi
