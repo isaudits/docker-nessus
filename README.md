99743b5307c80fae39e3bc49cb0d3b748d678bb4
Matt Jones <matt.jones@symphona.us>
2026-06-26 20:01:23 -0400

    refactor: update logging in Nessus update scripts and cron job

--- README.md
+++ README.md
@@ -1,28 +1,27 @@
 # docker-nessus
+
 Multi-arch (AMD64 + ARM64) Docker implementation of Nessus scanner with s6 overlay
 
---------------------------------------------------------------------------------
+---
 
 Copyright 2020
 
 Matthew C. Jones, CPA, CISA, OSCP, CCFE
 
-IS Audits & Consulting, LLC - <http://www.isaudits.com/>
+Symphona LLP / Symphona Technology - [https://symphona.us](https://symphona.us)
 
-TJS Deemer Dana LLP - <http://www.tjsdd.com/>
-
---------------------------------------------------------------------------------
+---
 
 Except as otherwise specified:
 
-This program is free software: you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free Software
-Foundation, either version 3 of the License, or (at your option) any later
+This program is free software: you can redistribute it and/or modify it under  
+the terms of the GNU General Public License as published by the Free Software  
+Foundation, either version 3 of the License, or (at your option) any later  
 version.
 
-This program is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+This program is distributed in the hope that it will be useful, but WITHOUT ANY  
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A  
 PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along with
-this program. If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License along with  
+this program. If not, see [http://www.gnu.org/licenses/](http://www.gnu.org/licenses/).
--- root/defaults/update-nessus.sh
+++ root/defaults/update-nessus.sh
@@ -1,20 +1,91 @@
 #!/command/with-contenv bash
 
-# Check registration status and update
-if /opt/nessus/sbin/nessuscli fetch --check; then
-    
-    /opt/nessus/sbin/nessuscli update --all
-    
-else
-    
-    if [ -n "${LICENSE}" ];then
-        echo "-- Registering as a Nessus Pro scanner and downloading updates"
-        /opt/nessus/sbin/nessuscli fetch --register "${LICENSE}"
-        /opt/nessus/sbin/nessuscli update --all
-    elif [ -n "${SECURITYCENTER}" ];then
-        echo "-- Registering as a SecurityCenter-linked scanner and downloading updates"
-        /opt/nessus/sbin/nessuscli fetch --security-center
-        /opt/nessus/sbin/nessuscli update --all
+set -o pipefail
+
+LOCK_DIR=/tmp/update-nessus.lock
+LOG_FILE=${UPDATE_NESSUS_LOG_FILE:-/var/log/update_nessus.log}
+STDOUT_ENABLED=true
+
+timestamp() {
+    date '+%Y-%m-%d %H:%M:%S %Z'
+}
+
+log() {
+    printf '[%s] %s\n' "$(timestamp)" "$*" >> "${LOG_FILE}"
+}
+
+stdout() {
+    if [ "${STDOUT_ENABLED}" = true ]; then
+        printf '%s\n' "$*"
     fi
-    
+}
+
+run_logged() {
+    log "Running: $*"
+    stdout "Running: $*"
+
+    "$@" 2>&1 | tr '\r' '\n' | while IFS= read -r line; do
+        if [ -n "${line}" ]; then
+            log "${line}"
+            stdout "${line}"
+        fi
+    done
+
+    return "${PIPESTATUS[0]}"
+}
+
+cleanup() {
+    rmdir "${LOCK_DIR}" 2>/dev/null || true
+}
+
+stdout_target=$(readlink /proc/$$/fd/1 2>/dev/null || true)
+if [ "${stdout_target}" = "${LOG_FILE}" ]; then
+    STDOUT_ENABLED=false
 fi
+
+if ! mkdir "${LOCK_DIR}" 2>/dev/null; then
+    log "Another Nessus update is already running; skipping this run"
+    stdout "Another Nessus update is already running; skipping this run"
+    exit 0
+fi
+
+trap cleanup EXIT
+
+log "========== Nessus update started =========="
+log "Timezone: ${TZ:-$(date +%Z)}"
+stdout "Nessus update started"
+
+exit_code=0
+
+# Check registration status and update
+if run_logged /opt/nessus/sbin/nessuscli fetch --check; then
+    run_logged /opt/nessus/sbin/nessuscli update --all || exit_code=$?
+elif [ -n "${LICENSE}" ]; then
+    log "Registering as a Nessus Pro scanner and downloading updates"
+    stdout "Registering as a Nessus Pro scanner and downloading updates"
+    run_logged /opt/nessus/sbin/nessuscli fetch --register "${LICENSE}" || exit_code=$?
+    if [ "${exit_code}" -eq 0 ]; then
+        run_logged /opt/nessus/sbin/nessuscli update --all || exit_code=$?
+    fi
+elif [ -n "${SECURITYCENTER}" ]; then
+    log "Registering as a SecurityCenter-linked scanner and downloading updates"
+    stdout "Registering as a SecurityCenter-linked scanner and downloading updates"
+    run_logged /opt/nessus/sbin/nessuscli fetch --security-center || exit_code=$?
+    if [ "${exit_code}" -eq 0 ]; then
+        run_logged /opt/nessus/sbin/nessuscli update --all || exit_code=$?
+    fi
+else
+    log "Nessus registration check failed and no LICENSE or SECURITYCENTER setting is configured"
+    stdout "Nessus registration check failed and no LICENSE or SECURITYCENTER setting is configured"
+    exit_code=1
+fi
+
+if [ "${exit_code}" -eq 0 ]; then
+    log "========== Nessus update finished successfully =========="
+    stdout "Nessus update finished successfully"
+else
+    log "========== Nessus update failed with exit code ${exit_code} =========="
+    stdout "Nessus update failed with exit code ${exit_code}"
+fi
+
+exit "${exit_code}"
